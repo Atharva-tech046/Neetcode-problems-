9d04587ffb81a2a7f98e6ec37d289d8a2c6077b2
Atharva Mohite <atharvam2017@gmail.com>
2026-06-21 13:38:45 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 90
+print(type(variable))
+variable = 8.68
+print(type(variable))
+variable = True
+print(type(variable))
+variable = 'Atharva'
+print(type(variable))
+variable = [1,2,3,4]
+print(type(variable))
