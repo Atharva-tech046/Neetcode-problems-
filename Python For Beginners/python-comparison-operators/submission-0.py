54da06fcb7e7b8f5c536eb2b2dedb89994890d98
Atharva Mohite <atharvam2017@gmail.com>
2026-07-16 11:22:13 +0530

    Add: python-comparison-operators - submission-0

---
+++ Python For Beginners/python-comparison-operators/submission-0.py
@@ -0,0 +1,42 @@
+def check_equal(x, y) -> bool:
+    result = (x==y)
+    return result
+
+def check_not_equal(x, y) -> bool:
+    result1 = (x!=y)
+    return result1
+
+def check_less_than(x, y) -> bool:
+    result2 = (x<y)
+    return result2
+
+def check_greater_than(x, y) -> bool:
+    result3 = (x>y)
+    return result3
+
+def check_less_than_or_equal(x, y) -> bool:
+    result4 = (x<=y)
+    return result4
+
+def check_greater_than_or_equal(x, y) -> bool:
+    result5 = (x>=y)
+    return result5
+
+# Don't change below this line
+print("2 is equal to 2:", check_equal(2, 2))
+print("-2 is equal to 2:", check_equal(-2, 2))
+
+print("-2 is not equal to 2:", check_not_equal(-2, 2))
+print("2 is not equal to 2:", check_not_equal(2, 2))
+
+print("2 is less than 3:", check_less_than(2, 3))
+print("3 is less than 3:", check_less_than(3, 3))
+
+print("3 is greater than 2:", check_greater_than(3, 2))
+print("3 is greater than 2:", check_greater_than(3, 3))
+
+print("3 is less than or equal to 3:", check_less_than_or_equal(3, 3))
+print("4 is less than or equal to 3:", check_less_than_or_equal(4, 3))
+
+print("3 is greater than or equal to 3:", check_greater_than_or_equal(3, 3))
+print("2 is greater than or equal to 3:", check_greater_than_or_equal(2, 3))
